ec3c4adbb32ca58dbebbbb447fff9e34880164ba
Aditya Eka Pranata <aepranata@gmail.com>
2023-08-13 03:35:35 +0700

    Kernel: Fix compile error on kernel 4.9
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- kernel/kernel_compat.c
+++ kernel/kernel_compat.c
@@ -3,10 +3,10 @@
 #include "linux/nsproxy.h"
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
 #include "linux/sched/task.h"
-#include "linux/uaccess.h"
 #else
 #include "linux/sched.h"
 #endif
+#include "linux/uaccess.h"
 #include "klog.h" // IWYU pragma: keep
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
